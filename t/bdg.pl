f84a13a0ed087ed005757c351a39cab531e40f9f
Bryan Gmyrek <bryangmyrek@gmail.com>
2012-09-06 02:01:12 -0700

    improve the talk, clean up bdg example

--- t/bdg.pl
+++ t/bdg.pl
@@ -1,20 +1,12 @@
 #!/usr/bin/perl
-use warnings;
-use strict;
-use Data::Dumper;
+use Modern::Perl;
 use lib '../';
+<snip how to use presentation builder>
 use Presentation::Builder;
-use Presentation::Builder::Types;
-use Presentation::Builder::Slide;
-use Presentation::Builder::Slide::Section;
-use Presentation::Builder::Template;
-
 my $pb = Presentation::Builder->new(
 	input_file    => "data/test-input.yaml", 
 	template_file => "data/index.base.html",
 	output_file   => "data/index.html",
-#	template_file => 'test-template.html',
 );
-
-#print $pb->content();
 $pb->generate();
+</snip>
